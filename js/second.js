f0af9fd9c4ddd50fddb9def2c0bdb4e5a8d70ad5
keeea <lexi.xiao@outlook.com>
2022-05-06 04:52:16 -0400

    Update second.js

--- js/second.js
+++ js/second.js
@@ -10,7 +10,7 @@ var employmentMap = L.map('employmentMap', {
   zoomDelta: 0.25
 }).setView(initialCenter, initialZoom);
 
-const baseLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+const baseLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   subdomains: 'abcd',
   minZoom: 0,
